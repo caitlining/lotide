d4e0e3b997afce70d7b2a53bbc9d494f32b18d4a
caitlining <caitlincing@gmail.com>
2019-09-18 00:57:07 +0000

    created function assertArraysEqual which takes two arrays and logs their assert message

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅ Assertation Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🔴 Assertation Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3, "dog"], [1, 2, 3, "dog"]);

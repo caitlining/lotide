6f0945d8092ad7d91bf98eace042264a0a82b17e
caitlining <caitlincing@gmail.com>
2019-09-18 20:27:10 +0000

    Adding file with countOnly function: this function takes in an array and an object and returns an new array

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍🏼Assertation Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`👎🏼Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item] += 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+// TEST CODE
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true});
+// note, our assertEqual function can only compare primitive values! Not objects, so we've broke down our tests
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+
+console.log(result1);

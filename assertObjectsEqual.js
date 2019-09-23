cd367ed5aee95cd1c77dfed1ca4e34df1e829077
caitlining <caitlincing@gmail.com>
2019-09-23 04:51:11 +0000

    Refactoring to include the definition on the previously made eqArrays function, as this is used in eqObjects which is also in this file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,14 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
 const eqObjects = function(object1, object2) {
   let object1KeyArray = Object.keys(object1);
   let object2KeyArray = Object.keys(object2);
@@ -9,17 +20,18 @@ const eqObjects = function(object1, object2) {
         return eqArrays(object1[keyName], object2[keyName]);
       } else {
         if (object1[keyName] !== object2[keyName]) {
-        return false;
+          return false;
         }
       }
-    } return true;
+    }
+    return true;
   }
 };
 
 
 //this will take in two objects and console.log an appropriate message to the console
 
-const assertObjectsEqual = function (inputObject, expectedObject) {
+const assertObjectsEqual = function(inputObject, expectedObject) {
   const inspect = require('util').inspect;
   if (eqObjects(inputObject, expectedObject)) {
     console.log(`✅ Assertation Passed: ${inspect(inputObject)} === ${inspect(expectedObject)}`);
@@ -28,5 +40,5 @@ const assertObjectsEqual = function (inputObject, expectedObject) {
   }
 };
 
-assertObjectsEqual({ a: 1, b: 2}, { a: 1, b: 2} );
-assertObjectsEqual({ a: 1, b: 5}, { a: 1, b: 2} );
+assertObjectsEqual({ a: 1, b: 2}, { a: 1, b: 2});
+assertObjectsEqual({ a: 1, b: 5}, { a: 1, b: 2});

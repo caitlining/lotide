ac1b88c836f0d2ba3a9372a24728579830d0fcf5
caitlining <caitlincing@gmail.com>
2019-09-23 04:40:28 +0000

    Refactoring to update assertEqual and pass eslint checks

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,16 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍🏼Assertation Passed: ${actual}  === ${expected}`);
+    console.log(`✅Assertation Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👎🏼Assertation Failed: ${actual} !== ${expected}`);
+    console.log(`🔴Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
 //make a function that returns an object
 //the function takes in a string
-//the returned object will have key-value pairs 
+//the returned object will have key-value pairs
 // those pairs will show the quantity of each letter in the given string
 // skip spaces
-
 //tip, you can use for...of loops with strings
 
 //this function works but I accidentally made it for counting words, not characters
@@ -24,38 +23,38 @@ const countWords = function (inputSentenceAsString) {
       outputObjectWithLetterQuantities[word] += 1;
     } else {
       outputObjectWithLetterQuantities[word] = 1;
-    }  
+    }
   } return (outputObjectWithLetterQuantities);
 };
 */
-
 //to make this work properly, I'm going to loop through my input string and make a new string that is just each character, sans spaces
 
 
-const countLetters = function (inputSentence) {
+const countLetters = function(inputSentence) {
   let outputObjectWithLetterQuantities = {};
   let inputSentenceNoSpaces = "";
   for (const character of inputSentence) {
     if (character !== " ") {
       inputSentenceNoSpaces += character;
-    } 
+    }
   } for (const character of inputSentenceNoSpaces) {
     if (outputObjectWithLetterQuantities[character]) {
       outputObjectWithLetterQuantities[character] += 1;
     } else {
       outputObjectWithLetterQuantities[character] = 1;
     }
-  } return outputObjectWithLetterQuantities;
+  }
+  return outputObjectWithLetterQuantities;
 };
 
 
-const results1 = countLetters("how are you today")
+const results1 = countLetters("how are you today");
 const results2 = countLetters("lighthouse in the house");
 
-//TESTS 
+//TESTS
 
-//console.log(results1);
-//console.log(results2);
+console.log(results1);
+console.log(results2);
 
 assertEqual(results1["a"], 2);
 assertEqual(results1["h"], 1);

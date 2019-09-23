22986010204d008b2c9a96592b51a49bfdf212a5
caitlining <caitlincing@gmail.com>
2019-09-23 19:28:46 +0000

    Refactoring eqArray.js to use module.export and require. Creating a test file to test eqArrays.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
@@ -17,14 +11,4 @@ const eqArrays = function(arrOne, arrTwo) {
   } return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]);// => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;

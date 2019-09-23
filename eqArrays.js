47b9907b13838e795ffb2907e1ba840a0b8235c3
caitlining <caitlincing@gmail.com>
2019-09-23 04:11:18 +0000

    Refactoring to include my refactored assertEqual code

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍🏼Assertation Passed: ${actual} === ${expected}`);
+    console.log(`✅Assertation Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👎🏼Assertation Failed: ${actual} !== ${expected}`);
+    console.log(`🔴Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 

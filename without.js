fdf6c9b9df75f5f5db9f185bdeff86b6ad1b5790
caitlining <caitlincing@gmail.com>
2019-09-23 04:25:49 +0000

    Refactoring: I found this function very difficult to implement when it was first assigned but upon reviewing today I was able to write a cleaner version

--- without.js
+++ without.js
@@ -17,25 +17,35 @@ const assertArraysEqual = function(arrOne, arrTwo) {
   }
 };
 
-const without = function (source, itemsToRemove) {
-  let cleanArray = [];
-  for (let i = 0; i < source.length; i++) {
-    let matchingElements = false;
-    for (let q = 0; q < itemsToRemove.length; q++) {
-      if (source[i] === itemsToRemove[q]) {
-        matchingElements = true;
-      }
-    } if (matchingElements === false) {
-      cleanArray.push(source[i]);
+const without = function(source, itemsToRemove) {
+  const cleanArray = [];
+  for (const testNum of source) {
+    if (!itemsToRemove.includes(testNum)) {
+      cleanArray.push(testNum);
     }
-  } return cleanArray;
+  }
+  return cleanArray;
 };
+
+// The function below works, but I made a cleaner version above
+// const without = function(source, itemsToRemove) {
+//   let cleanArray = [];
+//   for (let i = 0; i < source.length; i++) {
+//     let matchingElements = false;
+//     for (let q = 0; q < itemsToRemove.length; q++) {
+//       if (source[i] === itemsToRemove[q]) {
+//         matchingElements = true;
+//       }
+//     } if (matchingElements === false) {
+//       cleanArray.push(source[i]);
+//     }
+//   } return cleanArray;
+// };
 
 
 // TESTING
-
-/*console.log(without([1, 2, 3], [1]));
-console.log(without(["1", "2", "3"], [1, 2, "3"]));*/
+// console.log(without([1, 2, 3], [1]));
+// console.log(without(["1", "2", "3"], [1, 2, "3"]));
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
